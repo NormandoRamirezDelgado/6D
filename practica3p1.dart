d8d125ba2080544b692887eaa37b3d2d45f042d5
Normando Ramírez <nramirezd@cbtis236.edu.mx>
2025-02-19 11:37:58 -0600

    practica3

---
+++ practica3p1.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+void main(List<String> args) {
+  List<int> numeros = [];
+  String respuesta = '';
+  do {
+    print('Introducir un número entero:');
+    int numero = int.parse(stdin.readLineSync()!);
+    numeros.add(numero);
+    print('Desea continuar?');
+    respuesta = stdin.readLineSync()!;
+  } while (respuesta == 'si');
+  print(numeros);
+  Map<int, String> nombres = {
+    1 : 'Juan',
+    2 : 'Luisa',
+    3 : 'Alberto',
+    4 : 'Luis',
+    5 : 'Juana'
+  };
+  print(nombres);
+  print('Clave: ');
+  int clave = int.parse(stdin.readLineSync()!);
+  print('Valor: ');
+  String valor = stdin.readLineSync()!;
+  nombres[clave] = valor;
+  print(nombres);
+
+}
